0cb8704df6fa2235c24f6914f3dc8eaf6b2dadc6
Miguel Silva <miguel.silva.422@gmail.com>
2023-05-29 00:49:33 +0100

    Coupon controller checkCoupon method

--- app/Controllers/Http/CouponsController.ts
+++ app/Controllers/Http/CouponsController.ts
@@ -1,3 +1,31 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Coupon from 'App/Models/Coupon';
+import { DateTime } from 'luxon';
 
-export default class CouponsController {}
+export default class CouponsController {
+  public async checkCoupon({ request, response }: HttpContextContract) {
+    try {
+
+      const { couponCode } = request.body();
+
+      if (!couponCode) throw new Error('Coupon not found');
+      if (typeof couponCode !== 'string') throw new Error('Coupon not found');
+      if (couponCode.length !== 7) throw new Error('Coupon not found');
+      if (!couponCode.match(/^[A-Z]{7}$/)) throw new Error('Coupon not found');
+
+      // get coupon from database by code
+      const coupon = await Coupon.findBy('code', couponCode);
+
+      if (!coupon) throw new Error('Coupon not found');
+      if (coupon.validUntil < DateTime.now()) throw new Error('Coupon is expired');
+      if (coupon.usageCount >= coupon.usageLimit) throw new Error('Coupon usage limit reached');
+
+      return response.ok({ success: true, message: 'Coupon is valid', discount: coupon.discount });
+
+    } catch (error) {
+      // return error message
+      return response.badRequest({ error: error.message });
+    }
+  }
+}
